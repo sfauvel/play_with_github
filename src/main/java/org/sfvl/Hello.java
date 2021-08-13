186e45f9fe03bd6f07f7a3e4b92f4169806a562e
sfauvel <sfauvel@gmail.com>
2021-08-14 00:31:59 +0200

    Add workflow test_doc_update to launch when no source was modified

---
+++ src/main/java/org/sfvl/Hello.java
@@ -0,0 +1,7 @@
+package org.sfvl;
+
+public class Hello {
+    public void sayHello(String name) {
+        System.out.println("Hello " + name + " !");
+    }
+}
---
+++ src/test/java/org/sfvl/HelloTest.java
@@ -0,0 +1,14 @@
+package org.sfvl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    @Test
+    public void test_hello() {
+        final Hello hello = new Hello();
+        System.out.println("HelloTest.test_hello");
+    }
+}
