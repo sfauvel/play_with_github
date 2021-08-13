15d2ae880d4340bddf4f0025e159913857a7d0bc
sfauvel <sfauvel@gmail.com>
2021-08-14 00:34:52 +0200

    Change a file in src/main

--- src/main/java/org/sfvl/Hello.java
+++ src/main/java/org/sfvl/Hello.java
@@ -3,5 +3,6 @@ package org.sfvl;
 public class Hello {
     public void sayHello(String name) {
         System.out.println("Hello " + name + " !");
+        System.out.println("Change behavior");
     }
 }
