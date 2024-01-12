ac0c21fedc3571825ab49f0f1afcad6345cb816f
sfauvel <sfauvel@gmail.com>
2024-01-12 16:43:37 +0100

    Create a rust project

---
+++ src-wasm/src/lib.rs
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_Wasm
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
